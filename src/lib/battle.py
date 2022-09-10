96754d064ccb2816f19e84c147ab424ab851ab39
Alejo-Casta <alejo.casta.al@gmail.com>
2022-09-10 16:54:40 -0500

    Battle terminado

--- src/lib/battle.py
+++ src/lib/battle.py
@@ -1,3 +1,4 @@
+import random
 from dataclasses import dataclass
 from typing import List
 
@@ -46,4 +47,50 @@ def simulate_battle(p1: Pokemon, p2: Pokemon, random_seed: int = 0) -> BattleSum
     This function is provided a random_seed value, so that the battle always
     yields the same results.
     """
-    raise NotImplementedError
+    if p1.name == p2.name:
+        return BattleSummary(winner=p1, defeated=p2, rounds=[])
+    random.seed(random_seed)
+    winners = {p1.name: 0, p2.name: 0}
+    rounds = {p1.name: [], p2.name: []}
+    total_abilities = len(p1.abilities) + len(p2.abilities)
+    weights = [len(p1.abilities) / total_abilities,
+               len(p2.abilities) / total_abilities]
+
+    for i in range(0, 1000):
+        summary = generate_battle(p1=p1, p2=p2, weights=weights)
+        winner_name = summary.winner.name
+        winners[winner_name] += 1
+        if rounds[winner_name]:
+            if len(rounds[winner_name]) > len(summary.rounds):
+                rounds[winner_name] = summary.rounds
+        else:
+            rounds[winner_name] = summary.rounds
+
+    winner = p1 if winners[p1.name] >= winners[p2.name] else p2
+    defeated = p1 if winners[p1.name] <= winners[p2.name] else p2
+    winner_rounds = rounds[winner.name]
+    return BattleSummary(winner=winner, defeated=defeated, rounds=winner_rounds)
+
+
+def generate_battle(p1: Pokemon, p2: Pokemon, weights: List[float]) -> BattleSummary:
+    round_count = 0
+    rounds = []
+    healths = {p1.name: p1.health_points, p2.name: p2.health_points}
+    while 1:
+        attacker = random.choices([p1, p2], weights)[0]
+        defender = list(filter(lambda pokemon: (
+                pokemon != attacker), [p1, p2]))[0]
+        if not defender:
+            defender = p1
+        ability = random.choices(attacker.abilities)[0]
+        damage = attacker.attack * random.betavariate(1, 5)
+        healths[defender.name] -= damage
+        rounds.append(BattleRound(
+            attacker.name, defender.name, damage, ability))
+        round_count += 1
+        if healths[defender.name] <= 0:
+            return BattleSummary(winner=attacker, defeated=defender, rounds=rounds)
+        if round_count >= 100:
+            winner = attacker if healths[attacker.name] >= healths[defender.name] else defender
+            defeated = defender if healths[defender.name] >= healths[attacker.name] else attacker
+            return BattleSummary(winner=winner, defeated=defeated, rounds=rounds)
